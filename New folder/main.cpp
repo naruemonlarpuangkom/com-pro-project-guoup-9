ec579dbe7611f81cc85760891b61a8f3ee6cc62d
Sakuraii <pearl.k.sakura@gmail.com>
2022-01-28 15:54:52 +0700

    update main

--- New folder/main.cpp
+++ New folder/main.cpp
@@ -8,6 +8,8 @@ sqlite3* db;
 
 //prototype
 void allmenu();
+void showmenu();
+void addmenu();
 
 int main() 
 { 
@@ -25,13 +27,15 @@ int main()
     } else {
 
         cout << "Opened Database Successfully!" << endl; 
-        /*sqlite3_prepare( db, "SELECT * FROM menu;", -1, &stmt, NULL );//preparing the statement
+        sqlite3_prepare( db, "SELECT * FROM menu;", -1, &stmt, NULL );//preparing the statement
         sqlite3_step( stmt );//executing the statement
         char * str = (char *) sqlite3_column_text( stmt, 1 );///reading the 1st column of the result
-        //cout << str << endl;*/
+        cout << str << endl;
     }
-    
-    allmenu();
+    //showmenu();
+    //allmenu();
+    addmenu();
+
     // push close button
     // Close the connection
     sqlite3_close(db);
@@ -51,7 +55,7 @@ void allmenu(){
             case SQLITE_ROW:
             for(int i=0; i<3;i++){
                 text = sqlite3_column_text(stmt, i);
-                cout << text << endl;
+                cout << text ;
             }
                 break;
             
@@ -69,5 +73,39 @@ void allmenu(){
     sqlite3_finalize(stmt);
 
 }
+
+void showmenu(){
+    sqlite3_stmt * stmt;
+  
+    int id=0;
+
+    sqlite3_prepare( db, "SELECT * FROM menu;", -1, &stmt, NULL );//preparing the statement
+    const unsigned char* text;
+    
+    for(int j=0;j<=id;j++){
+        sqlite3_step(stmt);
+        if(j==id){
+            for(int i=0;i<3;i++){
+                text = sqlite3_column_text(stmt,i);
+                cout << text << " ";
+            }
+            break;
+        }
+    }
+    sqlite3_finalize(stmt);
+}
+
+void addmenu(){
+    sqlite3_stmt * stmt;
+  
+    sqlite3_prepare( db, "SELECT * FROM menu;", -1, &stmt, NULL );//preparing the statement
+    string name,price;
+
+    cin >> name >> price;
+
+    //string sqlstatement = "INSERT INTO menu (food_name, price) VALUES (name, price)";
+    string sqlstatement = "INSERT INTO menu (food_name, price) VALUES ('" + name + "','" + price + "');";
+    
+}
 //gcc sqlite3.c -c   ทำแบบนี้มันจะไม่ได้ exe แต่จะได้เป็น object file ที่ลิงค์เข้ากับโค้ดให้โปรแกรมเราไปเรียกใช้ตอน compile ครับ
-//g++ main.cpp sqlite3.o
+//how to run (safe file first) ; g++ main.cpp sqlite3.o  && ./a.exe
